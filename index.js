699f46069a2962eb17c728436f552af70b0796c4
Ngoc Tu <tdngoctu@gmail.com>
2019-09-29 01:25:12 +0700

    add main.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 app.set('view engine', 'ejs')
 app.set('views','./views')
+app.use(express.static('public'))
 app.listen(3000,()=>console.log('Server is running'))
 
 app.get('/',(req, res)=>res.render('home'))
