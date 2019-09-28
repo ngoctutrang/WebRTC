81d8a12f4ee566f0b429460f257f722d694f84a1
Ngoc Tu <tdngoctu@gmail.com>
2019-09-29 00:52:42 +0700

    SetUp Server

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+app.set('view engine', 'ejs')
+app.set('views','./views')
+app.listen(3000,()=>console.log('Server is running'))
+
+app.get('/',(req, res)=>res.render('home'))
